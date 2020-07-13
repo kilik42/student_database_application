20acaca724182b341c5ee2792bf6612406e44dfe
marvin evins <maevins@cps.edu>
2020-07-13 00:03:45 -0500

    implementing the enrollment method

--- src/student/Student.java
+++ src/student/Student.java
@@ -7,8 +7,8 @@ public class Student {
 	private String  lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses;
-	private int tuitionBalance;
+	private String courses = null;
+	private int tuitionBalance = 0;
 	private static int costOfCourse = 600;
 	private static int id = 1000;
 	
@@ -45,7 +45,25 @@ public class Student {
 	}
 	
 	//enroll in courses
-	
+	public void enroll(){
+		//get inside of a loop, user hits 0 when done enrolling
+		do{
+			System.out.println("Enter course to enroll (Q to quit): ");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q")){
+				courses = courses + "\n " + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+					
+			} else{
+				break;
+			}
+			
+		}while(1 != 0);
+			
+		System.out.println("Enrolled in : " + courses);
+		System.out.println(" Tuition Balance: " + tuitionBalance);
+	}
 	
 	//view balance
 	
--- src/student/StudentDatabaseApp.java
+++ src/student/StudentDatabaseApp.java
@@ -4,6 +4,7 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		Student stu1 = new Student();
+		stu1.enroll();
          //ask how many new users we want to add
 		
 		
